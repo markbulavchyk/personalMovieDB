871a258a171006743a0541d62cb0be624b639437
markbulavchyk <marksaitiev@gmail.com>
2024-12-21 17:55:41 -0700

    del method trim, lesson 4, mod js file

--- js/script.js
+++ js/script.js
@@ -1,77 +1,77 @@
 'use strict';
 
-let numberOfFilms;
-
-function start() {
-    numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели ?', '');
-
-    while(numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
-        numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели ?', '');
-    }
-}
-
-start();
-
 const personalMovieDB = {
-    count: numberOfFilms,
+    count: 0,
     movies: {},
     actors: {},
     genres: [],
-    privat: false
+    privat: false,
+    start: function () {
+        personalMovieDB.count = +prompt('Сколько фильмов вы уже посмотрели ?', '');
+
+        while (personalMovieDB.count == '' || personalMovieDB.count == null || isNaN(personalMovieDB.count)) {
+            personalMovieDB.count = +prompt('Сколько фильмов вы уже посмотрели ?', '');
+        }
+    },
+    rememberMyFilms: function () {
+        for (let i = 0; i < 2; i++) {
+            const a = prompt('Один из последних просмотренных фильмов?',''),
+            b = prompt('На сколько оцените его?','');
+            
+        
+            if (a != null && b != null &&  a != '' && b != '' && a.length < 50) {
+                personalMovieDB.movies[a] = b;
+                console.log('done');
+            } else {
+                console.log('error');
+                i--;
+            }
+        }
+    },
+    detectPersonalLevel: function () {
+        if (personalMovieDB.count < 10) {
+            console.log('Просмотрено довольно мало фильмов');
+        } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30 ){
+            console.log("Вы классический зритель");
+        } else if (personalMovieDB.count >= 30 ){
+            console.log("Вы киноман");
+        } else {
+            console.log("Произошла ошибка");
+        }
+    },
+
+    showMyDB: function (hidden) {
+         
+        if (!hidden) {
+            console.log(personalMovieDB);
+        }
+        // hidden аргумент в данном случаи свойство прайвет ,
+        // если прайвет не тру тобишь прайвет фолсе  мы показываем ДатаБейз
+ 
+    },
+    toggleVisibleMyDB: function () {
+        if (personalMovieDB.privat === false ) {
+            personalMovieDB.privat = true;
+        } else {
+            personalMovieDB.privat = false;
+        }
+    },
+
+    writeYourGenres: function () {
+        for (let i = 1; i <= 3; i++) {
+            let genre = prompt(`Ваш любимый жанр под номером ${i}`, ``);
+            if (genre === '' || genre == null) {
+                console.log('Вы ввели некорректные данные или не ввели их вовсе');
+                i--;
+            } else {
+                personalMovieDB.genres[i - 1] = genre;
+            }
+        }
+        personalMovieDB.genres.forEach((item, i) => {
+            console.log(`Любимый жанр ${i + 1} - это ${item}`);
+        });
+    }
 };
 
-function rememberMyFilms () {
-    for (let i = 0; i < 2; i++) {
-        const a = prompt('Один из последних просмотренных фильмов?','').trim(),
-        b = prompt('На сколько оцените его?','');
-        
-    
-        if (a != null && b != null &&  a != '' && b != '' && a.length < 50) {
-            personalMovieDB.movies[a] = b;
-            console.log('done');
-        } else {
-            console.log('error');
-            i--;
-        }
-    }
-}
-
-rememberMyFilms();
-
-function detectPersonalLevel() {
-    if (personalMovieDB.count < 10) {
-        console.log('Просмотрено довольно мало фильмов');
-    } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30 ){
-        console.log("Вы классический зритель");
-    } else if (personalMovieDB.count >= 30 ){
-        console.log("Вы киноман");
-    } else {
-        console.log("Произошла ошибка");
-    }
-}
-detectPersonalLevel();
-
-
-// function showMyDB () {
-//     if (personalMovieDB.privat === false) {
-//         console.log(personalMovieDB);
-//     }
-// }
-
-function showMyDB (hidden) {
-    if (!hidden) {
-        console.log(personalMovieDB);
-    }
-}
-// hidden аргумент в данном случаи свойство прайвет , ё
-// если прайвет не тру тобишь прайвет фолсе  мы показываем ДатаБейз
- 
-
-showMyDB(personalMovieDB.privat);
-
-function writeYourGenres () {
-    for (let i = 1; i <= 3; i++) {
-        personalMovieDB.genres[i - 1] = prompt(`Ваш любимый жанр под номером ${i}`, ``);
-    }
-}
-writeYourGenres();
+// personalMovieDB.toggleVisibleMyDB()
+// console.log(personalMovieDB);
